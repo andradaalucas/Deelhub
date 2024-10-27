60fd74fdd06f7d3ae02e3422ddd1e837d5e5b2a2
Lucas Andrada <andradalucaswork@gmail.com>
2024-10-27 15:59:43 -0300

    changes

--- components/customers/table-customers/data-table-header.tsx
+++ components/customers/table-customers/data-table-header.tsx
@@ -6,8 +6,8 @@ import { CreateForm } from "@/components/customers/form-create-customers/create-
 export function HeaderTable() {
   return (
     <div className="flex items-center justify-between py-4">
-      <OptionsTable />
-      <CreateForm />
+      {/* <OptionsTable />
+      <CreateForm /> */}
     </div>
   );
 }
