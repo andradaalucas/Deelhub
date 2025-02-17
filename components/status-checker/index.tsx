57797fddd3c316af9c581d4d88377851332880ed
Lucas Andrada <andradalucaswork@gmail.com>
2025-02-17 14:13:41 -0300

    changes

--- components/status-checker/index.tsx
+++ components/status-checker/index.tsx
@@ -36,6 +36,7 @@ export function StatusChecker({ slug }: StatusCheckerProps) {
   const [status, setStatus] = useState<{ label: string; color: string } | null>(
     null,
   );
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchStatus() {
@@ -56,11 +57,34 @@ export function StatusChecker({ slug }: StatusCheckerProps) {
       } catch (error) {
         console.error("Error fetching status:", error);
         setStatus(null);
+      } finally {
+        setLoading(false);
       }
     }
 
     fetchStatus();
   }, [slug]);
+
+  if (loading) {
+    return (
+      <a
+        className="inline-flex max-w-fit items-center gap-2 rounded-md border border-border px-3 py-1 text-sm font-semibold text-foreground/70 hover:bg-muted hover:text-foreground"
+        href={`https://${slug}.openstatus.dev`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Loading
+        <span className="relative flex h-2 w-2">
+          <span
+            className={`absolute inline-flex h-full w-full animate-ping rounded-full bg-[#f6a623] opacity-75 duration-1000`}
+          />
+          <span
+            className={`relative inline-flex h-2 w-2 rounded-full bg-[#f6a623]`}
+          />
+        </span>
+      </a>
+    );
+  }
 
   if (!status) return null;
 
