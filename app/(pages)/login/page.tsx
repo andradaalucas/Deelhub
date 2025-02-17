14c5b811f4161022fb194b65a0b07848ff10df11
Lucas Andrada <andradalucaswork@gmail.com>
2025-02-17 15:18:18 -0300

    dot pattern

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -4,8 +4,9 @@ import { Vercel, Nextjs, Supabase, TypeScript } from "@/components/ui/brands";
 
 export default function Page() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <section className="flex w-full flex-col gap-y-6 rounded-lg p-8 px-6 sm:w-3/4 md:w-2/3 lg:w-1/4">
+    <div className="relative flex h-screen items-center justify-center">
+      <div className="absolute inset-0 z-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:24px_24px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_50%,transparent_100%)]"></div>
+      <section className="z-10 flex w-full flex-col gap-y-6 rounded-lg p-8 px-6 sm:w-3/4 md:w-2/3 lg:w-1/4">
         {/* Header */}
         <header>
           <h1 className="mb-4 text-left text-3xl font-medium text-gray-800 dark:text-gray-200">
