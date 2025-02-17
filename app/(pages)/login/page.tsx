60c86d3697630905d6833106639e7c19231cbd37
Lucas Andrada <andradalucaswork@gmail.com>
2025-02-17 15:21:30 -0300

    dot patter none

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -4,9 +4,7 @@ import { StatusChecker } from "@/components/status-checker";
 export default function Page() {
   return (
     <div className="relative flex h-screen items-center justify-center">
-      <div className="absolute inset-0 z-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:24px_24px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_50%,transparent_100%)]"></div>
       <section className="z-10 flex w-full flex-col gap-y-6 rounded-lg p-8 px-6 sm:w-3/4 md:w-2/3 lg:w-1/4">
-        {/* Header */}
         <header>
           <h1 className="mb-4 text-left text-3xl font-medium text-gray-800 dark:text-gray-200">
             Login to Deelhub.
@@ -15,8 +13,6 @@ export default function Page() {
             Automate budgets, simplify client management, and stay organized.
           </p>
         </header>
-
-        {/* Login Button */}
         <div className="flex justify-center">
           <LoginButton />
         </div>
